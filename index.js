768e5df6979acab797be34cfbf08becf6ad130c1
Tiago <tiago.segato@ifb.edu.br>
2025-05-25 11:06:32 -0300

    primeira versão

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    console.log(`Entrou na Raiz!`);
+    res.send(`Bem-vindo a Home!!!`);
+});
+
+app.post('/', (req, res) => {
+    res.send(`Enviando o método POST, para inserir dados.`);
+});
+
+app.put('/', (req, res) => {
+    res.send(`Enviando o método PUT, para atualizar dados.`);
+});
+
+app.delete('/', (req, res) => {
+    res.send(`Enviando o método DELETE, para excluir dados.`);
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Aplicação rodando na porta ${PORT}`);
+});
+
+//https://www.youtube.com/watch?v=YeEX1s4M9xg&t=37s
