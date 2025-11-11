b17534873775a0ed4c9507938fbd58d9f2cc9a11
tiagosegato <tiago.segato@ifb.edu.br>
2025-11-11 09:56:42 -0300

    add comentários aula

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Exemplo de aplicação node para a criação e configuração inicial das rotas 
 Ter instalado o Node JS: https://nodejs.org/pt (optar pela versão LTS).
 
 **Comandos Básicos:**
-npm init -y => cria o package.json (com todas as configurações inciais)
-npm install express => instala a bilbioteca Express (e cria a node_modules)
-node index.js => executa o arquivo desejado
+npm init -y => cria o package.json (com todas as configurações inciais) <br>
+npm install express => instala a bilbioteca Express (e cria a node_modules) <br>
+node index.js => executa o arquivo desejado <br>
 
--- index.js
+++ index.js
@@ -1,25 +1,33 @@
-const express = require('express');
-const app = express();
-const PORT = 3000;
+//import express from "express"; Se importar assim tem que usar o modules
 
-app.get('/', (req, res) => {
-    console.log(`Entrou na Raiz!`);
-    res.send(`Bem-vindo a Home!!!`);
-});
+const express = require('express'); //importa o express para o projeto
 
-app.post('/', (req, res) => {
-    res.send(`Enviando o método POST, para inserir dados.`);
-});
+const app = express(); // atribui toda a "função" Express para a const app
 
-app.put('/', (req, res) => {
-    res.send(`Enviando o método PUT, para atualizar dados.`);
-});
+const PORT = 3000; // atribui a porta padrão a uma constante
 
-app.delete('/', (req, res) => {
-    res.send(`Enviando o método DELETE, para excluir dados.`);
-});
+//IMPLEMENTAÇÃO DAS ROTAS
+// req - informações de quem está enviando a requisição
+// res - o que a aplicação responde de volta
 
+// app.get('/', (req, res) => {
+//     console.log(`Entrou na Raiz!`);
+//     res.send(`Bem-vindo a Home!!!`);
+// });
 
+// app.post('/', (req, res) => {
+//     res.send(`Enviando o método POST, para inserir dados.`);
+// });
+
+// app.put('/', (req, res) => {
+//     res.send(`Enviando o método PUT, para atualizar dados.`);
+// });
+
+// app.delete('/', (req, res) => {
+//     res.send(`Enviando o método DELETE, para excluir dados.`);
+// });
+
+//Função que fica "ouvindo" tudo que acontece na porta definida
 app.listen(PORT, () => {
     console.log(`Aplicação rodando na porta ${PORT}`);
 });
